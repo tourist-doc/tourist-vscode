51743f1856befbdfe725c39996c5ba13a5067c54
Jason Fields <jasonfields4@gmail.com>
2018-11-12 18:49:30 -0500

    Clicking on a tourstop will now cause the editor to navigate to the correct file and position and show the correct message

--- src/extension.ts
+++ src/extension.ts
@@ -4,9 +4,10 @@
 import * as vscode from 'vscode';
 
 interface Tourstop {
-    filePath: string,
-    lineNumber: number,
+    title: string,
     message: string
+    filePath: string,
+    position: vscode.Position,
 }
 
 class Tour implements vscode.TreeDataProvider<Tourstop> {
@@ -18,8 +19,16 @@ class Tour implements vscode.TreeDataProvider<Tourstop> {
 
     onDidChangeTreeData?: vscode.Event<Tourstop | null | undefined> | undefined;
 
-    getTreeItem(element: Tourstop): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        return { label: element.message };
+    getTreeItem(element: Tourstop): vscode.TreeItem {
+        return {
+            label: element.title,
+            tooltip: element.message,
+            command: {
+                title: 'lol what?',
+                command: "extension.gotoTourStop",
+                arguments: [element]
+            }
+        };
     }
 
     getChildren(element?: Tourstop | undefined): vscode.ProviderResult<Tourstop[]> {
@@ -30,25 +39,37 @@ class Tour implements vscode.TreeDataProvider<Tourstop> {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    console.log('Congratulations, your extension "tourist" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.takeTour', () => {
         const tour = new Tour([{
-            filePath: '',
-            lineNumber: 25,
-            message: 'Tourstop #1'
+            title: 'Tourstop #1',
+            message: 'This is a message lol',
+            filePath: 'src/extension.ts',
+            position: new vscode.Position(25, 3)
         },
         {
-            filePath: '',
-            lineNumber: 10,
-            message: 'Tourstop #2'
+            title: 'activate()',
+            message: 'This function is called when the extension is activated!',
+            filePath: 'C:/Users/jason/Desktop/tourist-vscode/src/extension.ts',
+            position: new vscode.Position(40, 0)
         }]);
         vscode.window.createTreeView<Tourstop>('touristView', { treeDataProvider: tour });
-        vscode.window.showInformationMessage('Welcome to the tour lol');
+    });
+
+    context.subscriptions.push(disposable);
+
+    disposable = vscode.commands.registerCommand('extension.gotoTourStop', (tourstop: Tourstop) => {
+        const fileURI = vscode.Uri.file(tourstop.filePath);
+        vscode.workspace.openTextDocument(fileURI).then(doc => {
+            vscode.window.showTextDocument(doc).then(editor => {
+                editor.selection = new vscode.Selection(tourstop.position, tourstop.position)
+                vscode.window.showInformationMessage(tourstop.message);
+            });
+        }, (error: any) => {
+            vscode.window.showErrorMessage(`Unable to open file ${fileURI.fsPath}`);
+        });
     });
 
     context.subscriptions.push(disposable);
