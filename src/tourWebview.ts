0cfc28b3046788c2f77dccba20eed15eda5e903a
Jason Fields <jfields@broadwaytechnology.com>
2019-07-01 23:36:33 -0400

    Use text area for editing tour description
    Fixes #105

--- src/tourWebview.ts
+++ src/tourWebview.ts
@@ -9,11 +9,15 @@ import { TouristWebview } from "./webview";
 interface TourTemplateArgs {
   tf: TourFile;
   tour: Tour;
+  editingDescription?: string;
   showEditControls: boolean;
 }
 
 export class TourWebview {
   protected template: (args: TourTemplateArgs) => string;
+
+  /** Whether the body is currently being edited (the TextArea is showing) */
+  protected editingDescription?: string;
 
   public constructor(tmpl: string) {
     this.template = template(
@@ -29,8 +33,15 @@ export class TourWebview {
     panel.webview.html = this.template!({
       tf: tourState!.tourFile,
       tour: tourState!.tour,
+      editingDescription: this.editingDescription,
       showEditControls: config.showEditControls(),
     });
+  }
+
+  public setEditing(editing: boolean) {
+    this.editingDescription = editing
+      ? tourState!.tourFile.description || ""
+      : undefined;
   }
 
   public async handleMessage(message: any) {
@@ -42,10 +53,23 @@ export class TourWebview {
         await commands.mapRepo(message.repo);
         break;
       case "editDescription":
-        await commands.editDescription(tourState!.tourFile);
+        this.setEditing(true);
+        break;
+      case "editDescriptionCancel":
+        this.setEditing(false);
+        break;
+      case "editDescriptionSave":
+        this.setEditing(false);
+        if (message.newDescription !== undefined) {
+          await commands.editDescription(
+            tourState!.tourFile,
+            message.newDescription,
+          );
+        }
         break;
       case "deleteStop":
         await commands.deleteTourStop(tourState!.tour.stops[message.stopIndex]);
+        break;
     }
 
     await TouristWebview.refresh();
